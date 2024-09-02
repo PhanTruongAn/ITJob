cfdbf87127e754d68e555f7fa468021c46bdc42e
PhanTruongAn <phanan1000@gmail.com>
2024-09-02 17:59:57 +0700

    Decode token and protect end-point with token

--- backend/src/main/java/vn/phantruongan/backend/controller/AuthController.java
+++ backend/src/main/java/vn/phantruongan/backend/controller/AuthController.java
@@ -10,22 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.phantruongan.backend.dto.LoginDTO;
+import vn.phantruongan.backend.dto.ResLoginDTO;
+import vn.phantruongan.backend.util.SecurityUtil;
 
 @RestController
 public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityService;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityService = securityService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword());
 
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.ok().body(loginDto);
+
+        // Create jwt token
+        String access_token = securityService.createToken(authentication);
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
+        return ResponseEntity.ok().body(res);
     }
 }
